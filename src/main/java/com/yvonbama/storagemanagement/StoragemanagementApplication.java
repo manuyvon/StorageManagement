7edac7861de2fbcf71ce179a35049d0980add6ab
manuyvon <manuyvon@gmail.com>
2021-05-25 15:56:46 +0000

    First test setup

---
+++ src/main/java/com/yvonbama/storagemanagement/StoragemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.yvonbama.storagemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoragemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoragemanagementApplication.class, args);
+	}
+
+}
